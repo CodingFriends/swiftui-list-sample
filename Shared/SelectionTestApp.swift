2acf07d4dd2fbe0102d98a96d23c6bb7d4e30516
Gabriel Reimers <gabriel@coding-friends.com>
2022-04-04 09:15:17 +0200

    Initial Commit

---
+++ Shared/SelectionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelectionTestApp.swift
+//  Shared
+//
+//  Created by Gabriel Reimers on 04.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SelectionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
